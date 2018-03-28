91eb883fa5df8349e279f51669a4514ac4178965
Josh Southern <josh@joshsouthern.ca>
2018-03-28 15:22:25 -0700

    proxy with marlin's component

--- node-proxy/server/proxy-server.js
+++ node-proxy/server/proxy-server.js
@@ -6,18 +6,21 @@ const proxyHost = 'proxy';
 
 app.get('/restaurants/:id', async (req, res) => {
   const nearbyReq = await axios.get(`http://${proxyHost}:8888/nearby/restaurants/${req.params.id}`);
+  const galleryReq = await axios.get(`http://${proxyHost}:8888/gallery/restaurants/${req.params.id}`);
   const nearbyComponent = nearbyReq.data;
+  const galleryComponent = galleryReq.data;
   const html = `
     <html lang="en">
     <head>
       <meta charset="utf-8">
       <link rel="stylesheet" type="text/css" href="/styles.css">
+      <link rel="stylesheet" type="text/css" href="/gallery/styles.css">
       <link href="https://fonts.googleapis.com/css?family=Raleway:400,700|Roboto:400,700" rel="stylesheet">
       <!-- <link rel="shortcut icon" href="http://res.cloudinary.com/madlicorice/image/upload/v1520448614/WeGot-favicon.ico"> -->
       <title>WeGot</title>
     </head>
     <body>
-      <div id="gallery-app"></div>
+      <div id="gallery-app">${galleryComponent}</div>
       <div id="midsection">
         <div id="overview-app"></div>
         <div id="sidebar-app"></div>
@@ -27,6 +30,7 @@ app.get('/restaurants/:id', async (req, res) => {
       </div>
 
       <script src="/nearby/bundle.js"></script>
+      <script src="/gallery/bundle.js"></script>
       <!-- <script src="/overview/bundle.js"></script> -->
       <!-- <script src="/sidebar/bundle.js"></script> -->
       <!-- <script src="/recommendations/bundle.js"></script> -->
