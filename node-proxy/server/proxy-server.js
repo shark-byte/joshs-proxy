6c16a0d643c6e3dec75a62b0dfe6000048280bf3
Josh Southern <josh@joshsouthern.ca>
2018-03-28 19:51:22 -0700

    working with sidebar component

--- node-proxy/server/proxy-server.js
+++ node-proxy/server/proxy-server.js
@@ -7,14 +7,17 @@ const proxyHost = 'proxy';
 app.get('/restaurants/:id', async (req, res) => {
   const nearbyReq = await axios.get(`http://${proxyHost}:8888/nearby/restaurants/${req.params.id}`);
   const galleryReq = await axios.get(`http://${proxyHost}:8888/gallery/restaurants/${req.params.id}`);
+  const sidebarReq = await axios.get(`http://${proxyHost}:8888/sidebar/restaurants/${req.params.id}`);
   const nearbyComponent = nearbyReq.data;
   const galleryComponent = galleryReq.data;
+  const sidebarComponent = sidebarReq.data;
   const html = `
     <html lang="en">
     <head>
       <meta charset="utf-8">
       <link rel="stylesheet" type="text/css" href="/styles.css">
       <link rel="stylesheet" type="text/css" href="/gallery/styles.css">
+      <link rel="stylesheet" type="text/css" href="/sidebar/styles.css">
       <link href="https://fonts.googleapis.com/css?family=Raleway:400,700|Roboto:400,700" rel="stylesheet">
       <!-- <link rel="shortcut icon" href="http://res.cloudinary.com/madlicorice/image/upload/v1520448614/WeGot-favicon.ico"> -->
       <title>WeGot</title>
@@ -23,7 +26,7 @@ app.get('/restaurants/:id', async (req, res) => {
       <div id="gallery-app">${galleryComponent}</div>
       <div id="midsection">
         <div id="overview-app"></div>
-        <div id="sidebar-app"></div>
+        <div id="sidebar-app">${sidebarComponent}</div>
       </div>
       <div id="recommendations-app">
         ${nearbyComponent}
@@ -32,7 +35,7 @@ app.get('/restaurants/:id', async (req, res) => {
       <script src="/nearby/bundle.js"></script>
       <script src="/gallery/bundle.js"></script>
       <!-- <script src="/overview/bundle.js"></script> -->
-      <!-- <script src="/sidebar/bundle.js"></script> -->
+      <script src="/sidebar/bundle.js"></script>
       <!-- <script src="/recommendations/bundle.js"></script> -->
     </body>
     </html>
